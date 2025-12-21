2ef185afbe52b229e3c5925dafd0db917d91c554
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-12-21 17:37:15 +0100

    feat: Add navigation bar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/app/components/web/navbar";
+import { Navbar } from "@/components/web/navbar";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
+            <Navbar />
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/web/navbar.tsx
@@ -0,0 +1,43 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/web/theme-toggle";
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items-center gap-8">
+        <Link href="/">
+          <h1 className="text-3xl font-bold">
+            Next<span className="text-blue-500">Pro</span>
+          </h1>
+        </Link>
+
+        <div className="flex items-center gap-2">
+          <Link className={buttonVariants({ variant: "ghost" })} href="/">
+            Home
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/blog">
+            Blog
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/create">
+            Create
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Link className={buttonVariants()} href="/auth/sign-up">
+          Sign up
+        </Link>
+        <Link
+          className={buttonVariants({ variant: "outline" })}
+          href="/auth/login"
+        >
+          Login
+        </Link>
+
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
+}
