9cd23f5f7bd52d57bb77f90b47fe1e1763bbd76d
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-12-30 16:35:57 +0100

    feat: Add toaster to show logout success message and redirect to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { ConvexClientProvider } from "@/components/web/ConvexClientProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({
             <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
               {children}
             </main>
+            <Toaster closeButton />
           </ConvexClientProvider>
         </ThemeProvider>
       </body>
