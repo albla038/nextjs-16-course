a053fd1e704f33612b4e63bd7692d62f64d463da
Albert Lännerström <albert.lannerstrom@gmail.com>
2026-01-05 11:43:20 +0100

    feat: Add dynamic blog ID page base

---
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -0,0 +1,72 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { fetchQuery } from "convex/nextjs";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface PageRouteProps {
+  params: Promise<{
+    postId: Id<"posts">;
+  }>;
+}
+
+export default async function PostIdRoute({ params }: PageRouteProps) {
+  const { postId } = await params;
+
+  const post = await fetchQuery(api.posts.getPostById, { postId });
+
+  if (!post) {
+    return (
+      <div>
+        <h1 className="text-6xl font-extrabold text-red-500 py-20">
+          No post found
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto py-8 px-4 animate-in fade-in duration-500 relative">
+      <Link
+        href="/blog"
+        className={buttonVariants({ variant: "outline", className: "mb-4" })}
+      >
+        <ArrowLeft className="size-4" />
+        Back to Blog
+      </Link>
+
+      <div className="relative w-full h-[400px] mb-8 rounded-xl overflow-hidden shadow-sm">
+        <Image
+          src={
+            post.imageUrl ??
+            "https://images.unsplash.com/photo-1761839258753-85d8eecbbc29?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          alt={post.title}
+          fill
+          className="object-cover hover:scale-105 transition-transform duration-500"
+        />
+      </div>
+
+      <div className="gap-y-4 flex flex-col">
+        <h1 className="text-4xl font-bold tracking-tight text-foreground">
+          {post.title}
+        </h1>
+
+        <p className="text-sm text-muted-foreground">
+          Posted on: {new Date(post._creationTime).toLocaleDateString("en-US")}
+        </p>
+      </div>
+
+      <Separator className="my-8" />
+
+      <p className="text-lg leading-relaxed text-foreground/90 whitespace-pre-wrap">
+        {post.body}
+      </p>
+
+      <Separator className="my-8" />
+    </div>
+  );
+}
